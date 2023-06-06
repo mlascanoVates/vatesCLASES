45d76ea34e73443ed37d61e63682db29b79daccb
mlascanoVates <mlascano@vates.com>
2023-06-06 11:24:22 -0300

    se agrega clase13

---
+++ clase13/clase13/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("El profe trabaja en el algoritmo de los números primos");
+
+        new Random().ints(100,1,245);
+
+        a un flu
+    }
+
+
+}
