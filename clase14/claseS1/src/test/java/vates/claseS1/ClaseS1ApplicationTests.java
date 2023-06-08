c5364b6e7979d0e9bdf4c787b94e4b8eb4f3dfaa
Emilia Lascano <mlascano@vates.com>
2023-06-08 17:55:52 -0300

    agrego tp

---
+++ clase14/claseS1/src/test/java/vates/claseS1/ClaseS1ApplicationTests.java
@@ -0,0 +1,13 @@
+package vates.claseS1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaseS1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
